7a14041b7ff706041649d95494012a60133d35a7
Simon Wögerbauer <simonwoegerbauer@outlook.com>
2025-08-31 12:08:09 +0200

    Actually working basic functionality now

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use an official Gradle image as a base image (or openjdk if you prefer)
-FROM gradle:7.5-jdk17 AS build
+FROM gradle:8.14.3-jdk24-corretto-al2023 AS build
 
 # Set the working directory in the container
 WORKDIR /app
@@ -7,8 +7,8 @@ WORKDIR /app
 # Copy the entire Gradle project, including the gradle wrapper
 COPY . .
 
-# Run the Gradle build (this will use gradlew wrapper)
-RUN ./gradlew build --no-daemon
+# Run the Gradle build
+RUN gradle clean build --info --no-daemon
 
 # Remove the 'src' directory to save space
 RUN rm -rf src
--- src/main/java/eu/simonw/texbot/TexBot.java
+++ src/main/java/eu/simonw/texbot/TexBot.java
@@ -4,9 +4,15 @@ import eu.simonw.texbot.commands.MathsSlashCommand;
 import eu.simonw.texbot.tex.TexHandler;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,14 +24,35 @@ public static final Logger LOGGER = LoggerFactory.getLogger(TexBot.class);
     public static void main(String[] args) {
         final String TOKEN = System.getenv("TOKEN");
         if (TOKEN == null || TOKEN.trim().equalsIgnoreCase("")) {
-            LOGGER.error("Token is invalid {}", TOKEN);
-            LOGGER.error("Killing bot");
+            LOGGER.error("Token is invalid or empty: {}", TOKEN);
             return;
         }
         LOGGER.info("Token set with last 4 characters: {}", TOKEN.substring(TOKEN.length() - 4));
+
+
+        final List<GatewayIntent> intents = List.of(
+                GatewayIntent.MESSAGE_CONTENT);
+        JDA jda = JDABuilder.
+                createLight(TOKEN).
+                enableIntents(intents)
+                .setAutoReconnect(true)
+                .setActivity(Activity.watching("You do maths"))
+                .setCompression(Compression.ZLIB)
+                .setLargeThreshold(50)
+                .setStatus(OnlineStatus.ONLINE)
+                .setBulkDeleteSplittingEnabled(false)
+                .setChunkingFilter(ChunkingFilter.NONE)
+                .setMemberCachePolicy(MemberCachePolicy.NONE)
+                .disableCache(
+                        CacheFlag.ACTIVITY,
+                        CacheFlag.EMOJI,
+                        CacheFlag.CLIENT_STATUS,
+                        CacheFlag.STICKER,
+                        CacheFlag.VOICE_STATE
+                )
+                .build();
         TexHandler handler = new TexHandler();
         LOGGER.info("{}", handler.setup_file("tex/template_standalone.tex"));
-        JDA jda = JDABuilder.createLight(TOKEN).enableIntents(List.of(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.DIRECT_MESSAGES)).build();
         jda.addEventListener(new MathsSlashCommand(jda, handler));
     }
 }
